9cdfb758aecc8bc7b62d9481911641fb95a77742
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-05-03 12:15:03 +0300

    Fix some stylistic and syntactic issues
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/absent.pp
+++ manifests/absent.pp
@@ -7,6 +7,9 @@ class monit::absent inherits monit::params {
 
     if $facts['os']['family'] == 'Debian' {
         case $facts['os']['distro']['codename'] {
+            default: {
+                # Do nothing
+            }
             /(stretch|xenial)/: {
                 file { '/etc/monit/conf.d':
                     ensure       => 'absent',
