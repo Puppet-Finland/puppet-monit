100c8d13d66b947d429d1dfd8d60a5953ce612e9
Samuli Seppänen <samuli@openvpn.net>
2020-02-25 10:47:43 +0200

    Fix issues reported by "pdk validate"
    This involves upgrading the init_spec.rb to new default PDK format with minor
    additions
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'monit' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      extra_facts = if os_facts[:osfamily] == 'RedHat'
+                      { servermonitor: 'root@localhost', lsbdistcodename: 'RedHat' }
+                    else
+                      { servermonitor: 'root@localhost' }
+                    end
 
-  let(:title) { 'monit' }
-  let(:node) { 'test.example.org' }
-  let(:facts) { {
-    :operatingsystem => 'Ubuntu',
-    :osfamily        => 'Debian',
-    :lsbdistcodename => 'bionic',
-    :servermonitor   => 'root@localhost',
-  } }
+      let(:facts) { os_facts.merge(extra_facts) }
 
-  it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile }
+    end
+  end
 end
